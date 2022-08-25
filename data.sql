253078d3cafdaf475745bcaa4b507587a56ba4e0
Francisco Contreras <gamedxs@gmail.com>
2022-08-24 23:25:05 -0400

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,13 @@
+INSERT INTO folder(name, description, label, path, level) VALUES
+    ('possidere-group', '', 'possidere_group','possidere_group',1),
+    ('Estandar Nobilis', '', 'estandar_nobilis','estandar_nobilis',1),
+    ('Programación', '', 'programacion','estandar_nobilis.programacion',2),
+    ('Capital Humano', '', 'capital_humano','capital_humano',1),
+    ('Caaja los Andes', '', 'caja_los_andes','capital_humano.caja_los_andes',2)
+    
+ 
+    INSERT INTO folder(name, description, label, path, level) VALUES
+    ('Comercial', '', 'comercial','comercial',1),
+    ('Santander', '', 'santander','comercial.santander',2),
+    ('Capital Humano', '', 'capital_humano','comercial.capital_humano',2),
+    ('RRHH', '', 'rrhh','comercial.capital_humano.rrhh',3)
